416fb688d6fd05935b382d96a607213c25c35edc
Joy Developing <joydeveloping@gmail.com>
2015-01-19 13:54:20 +0300

    jdlib : using currying from jderlang-jdlib_currying repository

--- apps/jdlib/src/jdlib.erl
+++ apps/jdlib/src/jdlib.erl
@@ -8,7 +8,7 @@
 
 % Export.
 -export([fst/1, snd/1,
-         flip/1, curry/2, curry_1/2]).
+         flip/1]).
 
 %---------------------------------------------------------------------------------------------------
 % Types.
@@ -58,53 +58,6 @@ flip(F) when is_function(F, 2) ->
     fun(X, Y) -> F(Y, X) end;
 flip(T) ->
     throw({badarg, T}).
-
-%---------------------------------------------------------------------------------------------------
-% Haskell like curry function.
-%---------------------------------------------------------------------------------------------------
-
--spec curry(fun(), list()) -> fun((list()) -> term()).
-%% @doc
-%% Haskell like curry function.
-%% Agruments of this funtion are:
-%%   - function f of n arguments,
-%%   - list args of first m actual arguments (m <= n).
-%% Result of this function is new function g with n - m remain arguments new_args collected in list.
-%% g new_args = f args
-%%
-%% Note!
-%% After curring new function take not several arguments (remain arguments), but
-%% single argument - list of remain arguments.
-%% For example:
-%% F = fun(A, B, C) -> A + B + C end.
-%% G = curry(F, [5]).
-%% G is not function fun(B, C), but function fun([B, C]).
-curry(F, Args) when (is_function(F) andalso is_list(Args)) ->
-    {arity, Arity} = erlang:fun_info(F, arity),
-    Args_Count = length(Args),
-
-    % We can curry only if Args_Count <= Arity.
-    if
-        Args_Count < Arity ->
-            fun(Remain) ->
-                apply(F, Args ++ Remain)
-            end;
-        Args_Count =:= Arity ->
-            apply(F, Args);
-        true ->
-            throw({too_many_arguments, Args})
-    end;
-
-curry(F, Args) ->
-    throw({badarg, {F, Args}}).
-
-%---------------------------------------------------------------------------------------------------
-
--spec curry_1(fun(), term()) -> fun().
-%% @doc
-%% Curry case with one given actual argument.
-curry_1(F, T) ->
-    curry(F, [T]).
 
 %---------------------------------------------------------------------------------------------------
 
--- apps/jdlib/test/jdlib_test.erl
+++ apps/jdlib/test/jdlib_test.erl
@@ -12,7 +12,7 @@
 % Functions import.
 -import(jdlib,
         [fst/1, snd/1,
-         flip/1, curry/2, curry_1/2]).
+         flip/1]).
 
 %---------------------------------------------------------------------------------------------------
 % Tests.
@@ -47,22 +47,6 @@ flip_test() ->
     Flip_Minus_F = flip(Minus_F),
     ?assertEqual(5, Minus_F(10, 5)),
     ?assertEqual(-5, Flip_Minus_F(10, 5)),
-    ok.
-
-%---------------------------------------------------------------------------------------------------
-
--spec curry_test() -> ok.
-%% @doc
-%% Function curry_test.
-curry_test() ->
-    ?assertThrow({badarg, _}, curry(1, 2)),
-    F = fun(A, B, C) -> A * B * C end,
-    ?assertThrow({too_many_arguments, _}, curry(F, [1, 2, 3, 4])),
-    ?assertEqual(24, curry(F, [2, 3, 4])),
-    G = curry(F, [2]),
-    H = curry(F, [2, 5]),
-    ?assertEqual(30, G([3, 5])),
-    ?assertEqual(100, H([10])),
     ok.
 
 %---------------------------------------------------------------------------------------------------
