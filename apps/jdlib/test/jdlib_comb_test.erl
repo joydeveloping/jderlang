bf0c20f3c4c7062370d88b24caa9adc1efb00575
Joy Developing <joydeveloping@gmail.com>
2015-03-22 13:50:06 +0300

    jdlib_comb : c test

--- apps/jdlib/test/jdlib_comb_test.erl
+++ apps/jdlib/test/jdlib_comb_test.erl
@@ -24,6 +24,12 @@ c_test() ->
     ?assertEqual([0, 1, 4, 6, 4, 1, 0],
                  [c(-3, 4), c(0, 4), c(1, 4), c(2, 4), c(3, 4), c(4, 4), c(5, 4)]),
     ?assertEqual([0, 0], [c(-5, 10), c(5, -10)]),
+
+    % 10-th line of Pascal triangle.
+    ?assertEqual(jdlib_math:npow(2, 10),
+                 lists:sum(lists:map(jdlib_currying:curry_1(jdlib:flip(fun jdlib_comb:c/2), 10),
+                           lists:seq(0, 10)))),
+
     ok.
 
 %---------------------------------------------------------------------------------------------------
