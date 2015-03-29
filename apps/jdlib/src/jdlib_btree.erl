0406bbb7c49810e73639724e52eb4c0fc45024c6
Joy Developing <joydeveloping@gmail.com>
2015-03-29 16:44:58 +0300

    tree : insert and find

--- apps/jdlib/src/jdlib_btree.erl
+++ apps/jdlib/src/jdlib_btree.erl
@@ -7,7 +7,8 @@
 -module(jdlib_btree).
 
 % Export.
--export([init/1, data/1, left/1, right/1]).
+-export([init/3, init/1, data/1, left/1, right/1,
+         insert/3, insert/2, is_in/3, is_in/2]).
 
 %---------------------------------------------------------------------------------------------------
 % Constants and macroses.
@@ -20,10 +21,13 @@
 % Tree type.
 -export_type([btree/0]).
 
+% Data type.
+-type data() :: term().
+
 % Define binary tree as record.
 -record(btree,
 {
-    data :: term(),
+    data :: data(),
     left :: btree() | null,
     right :: btree() | null
 }).
@@ -35,15 +39,23 @@
 % Create and accessfFunctions.
 %---------------------------------------------------------------------------------------------------
 
--spec init(D :: term()) -> btree().
+-spec init(D :: data(), L :: btree(), R :: btree()) -> btree().
 %% @doc
 %% Initialize new binary tree.
-init(D) ->
-    #btree{data = D, left = null, right = null}.
+init(D, L, R) ->
+    #btree{data = D, left = L, right = R}.
 
 %---------------------------------------------------------------------------------------------------
 
--spec data(T :: btree()) -> term().
+-spec init(D :: data()) -> btree().
+%% @doc
+%% Initialize new binary tree.
+init(D) ->
+    init(D, null, null).
+
+%---------------------------------------------------------------------------------------------------
+
+-spec data(T :: btree()) -> data().
 %% @doc
 %% Get binary tree data.
 data(#btree{data = D}) ->
@@ -64,6 +76,61 @@ left(#btree{left = L}) ->
 %% Get right subtree.
 right(#btree{right = R}) ->
     R.
+
+%---------------------------------------------------------------------------------------------------
+% Insert and find functions.
+%---------------------------------------------------------------------------------------------------
+
+-spec insert(T :: btree(), New :: data(),
+             Is_Gt_Fun :: fun((data(), data()) -> boolean())) -> btree().
+%% @doc
+%% Insert data in binary tree.
+insert(null, New, _) ->
+    init(New);
+insert(#btree{data = D, left = L, right = R}, New, Is_Gt_Fun) ->
+    Is_Gt = Is_Gt_Fun(New, D),
+    if
+        Is_Gt ->
+            init(D, L, insert(R, New, Is_Gt_Fun));
+        true ->
+            init(D, insert(L, New, Is_Gt_Fun), R)
+    end.
+
+%---------------------------------------------------------------------------------------------------
+
+-spec insert(T :: btree(), New :: data()) -> btree().
+%% @doc
+%% Insert data in binary tree.
+insert(T, New) ->
+    insert(T, New, fun(X, Y) -> X > Y end).
+
+%---------------------------------------------------------------------------------------------------
+
+-spec is_in(T :: btree(), Data :: data(),
+            Is_Gt_Fun :: fun((data(), data()) -> boolean())) -> boolean().
+%% @doc
+%% Check if data is in binary tree.
+is_in(null, _, _) ->
+    false;
+is_in(#btree{data = D, left = L, right = R}, Data, Is_Gt_Fun) ->
+    Is_Gt = Is_Gt_Fun(Data, D),
+    Is_Lt = Is_Gt_Fun(D, Data),
+    if
+        Is_Gt ->
+            is_in(R, Data, Is_Gt_Fun);
+        Is_Lt ->
+            is_in(L, Data, Is_Gt_Fun);
+        true ->
+            true
+    end.
+
+%---------------------------------------------------------------------------------------------------
+
+-spec is_in(T :: btree(), Data :: data()) -> boolean().
+%% @doc
+%% Check if data is in binary tree.
+is_in(T, Data) ->
+    is_in(T, Data, fun(X, Y) -> X > Y end).
 
 %---------------------------------------------------------------------------------------------------
 
